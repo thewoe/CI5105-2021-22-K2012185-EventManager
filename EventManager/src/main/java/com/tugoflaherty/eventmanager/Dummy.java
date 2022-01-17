18ef84c03b2661bc0939c2c25c9bf43b67463e5a
Tug O'Flaherty <K2012185@kingston.ac.uk>
2022-01-17 15:56:19 +0000

    P1 - Setting up an empty Maven NetBeans Project (EventManager) in GitLab Repository (EventManager)

---
+++ EventManager/src/main/java/com/tugoflaherty/eventmanager/Dummy.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tugoflaherty.eventmanager;
+
+/**
+ *
+ * @author tugso
+ */
+public class Dummy {
+    // Do not use - just for fooling GitLab to keep this package
+}
