82949af96ae480fcd2fa2f07b5a7a241c8aca870
Tug O'Flaherty <K2012185@kingston.ac.uk>
2022-02-07 15:55:46 +0000

    P2RUx - Created MyHandler class and controller package to allow the view to interface with the model. Working as intended.

---
+++ EventManager/src/main/java/com/tugoflaherty/eventmanager/controller/MyHandler.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tugoflaherty.eventmanager.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author tugso
+ */
+public class MyHandler implements ActionListener {
+    //Class Methods
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        //add in all the button clicky things here.
+    }
+}
